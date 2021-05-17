b86fc3a2b43bf067b32182aebf15c18adec2e5a4
remind1990 <tigerstylest00@gmail.com>
2021-05-17 20:54:39 +0300

    chages for text

--- new.js
+++ new.js
@@ -2,4 +2,5 @@ document.addEventListener("DOMContentLoaded", () => {
   let div = document.createElement("div");
   div.innerHTML = "hello wewesadsa";
   document.body.append(div);
+  var cus = "simba";
 });
